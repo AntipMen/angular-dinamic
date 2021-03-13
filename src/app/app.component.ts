f9aa9cd780eaa6b107480fa796f220dd8870d355
Anna <Anna.Antipova794gmail.com>
2021-03-13 21:52:12 +0200

    add practice lesson with angular dynamic component and other

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import {Component, ComponentFactoryResolver, ViewChild} from '@angular/core';
+import {ModalComponent} from './modal/modal.component';
+import {RefDirective} from './ref.directive';
+import {Meta, Title} from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss'],
 })
+
 export class AppComponent {
-  title = 'angular-dinamic';
+
+  @ViewChild(RefDirective) refDir: RefDirective;
+
+  constructor(
+    private resolver: ComponentFactoryResolver,
+    private title: Title,
+    private meta: Meta) {
+    // const t = title.getTitle();
+    // console.log(t);
+    title.setTitle('App Component Page');
+    meta.addTags([
+      {name: 'keywords', content: 'angular,google'},
+      {name: 'description', content: 'appcomponent'}
+    ]);
+  }
+
+  showModal(): void {
+    const modalFactory = this.resolver.resolveComponentFactory(ModalComponent);
+    this.refDir.containerRef.clear();
+
+    const component = this.refDir.containerRef.createComponent(modalFactory);
+
+    component.instance.title = 'Dynamic title';
+    component.instance.close.subscribe(() => {
+      this.refDir.containerRef.clear();
+    });
+  }
 }
